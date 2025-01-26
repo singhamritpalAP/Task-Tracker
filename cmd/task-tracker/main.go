3863cc8cb80bf5173d01761ecd26fa0348d577a4
amritsinghnpci@gmail.com <amritsinghnpci@gmail.com>
2025-01-27 00:06:17 +0530

    [added comments in main]

--- cmd/task-tracker/main.go
+++ cmd/task-tracker/main.go
@@ -11,20 +11,29 @@ import (
 )
 
 func main() {
-
+	// Initialize the database wrapper to manage database connections and operations
 	dbWrapper, err := relationaldatabase.NewDbWrapper()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // Log and terminate if there is an error initializing the database wrapper
 	}
+
+	// Create a new adapter for the database service layer
 	database := iniservice.NewAdapter(dbWrapper)
-	// initializing services
+
+	// Initialize application services, passing in the database adapter
 	api := tasktrackerservice.NewApplication(database)
-	// initializing routers
+
+	// Set up the router for handling HTTP requests and define task tracker routes
 	router := routes.NewTaskTrackerRouter().SetTaskTrackerRoutes(api)
+
+	// Start the HTTP server on the specified port, using the defined router for handling requests
 	err = http.ListenAndServe(constants.ApplicationPort, router)
 	if err != nil {
-		log.Fatal("Error starting server", err)
+		log.Fatal("Error starting server", err) // Log any errors that occur while starting the server
 		return
 	}
-	//server.New(router, constants.DevEnvironment, constants.ApplicationPort, shutDown).Run()
+
+	// for using a custom server implementation with shutdown capabilities,
+	// minor changes needed in custom server logic to use custom server
+	// server.New(router, constants.DevEnvironment, constants.ApplicationPort, shutDown).Run()
 }
